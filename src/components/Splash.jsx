d449726b24d76dffd7c94499fd923026958f7582
marco9lli87-prog <marco9lli87@gmail.com>
2025-12-15 00:32:23 +0100

    Add Splash component with timer and click handler

---
+++ src/components/Splash.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+
+export default function Splash({ onFinish }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      localStorage.setItem('seenSplash', 'true')
+      onFinish()
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [onFinish])
+
+  return (
+    <div
+      onClick={onFinish}
+      style={{
+        position: 'fixed',
+        inset: 0,
+        background: '#000',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 9999
+      }}
+    >
+      <img
+        src="/logo.png"
+        alt="In Cucina con Glò"
+        className="splash-logo"
+      />
+    </div>
+  )
+}
