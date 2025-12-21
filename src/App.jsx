6f492b7e006764e9ba71d08e59d9616a43e52d34
marco9lli87-prog <marco9lli87@gmail.com>
2025-12-22 00:23:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Home"
+
 import AdminDashboard from "./pages/AdminDashboard"
-
-/* Placeholder temporanei */
-function AdminOrders() {
-  return <div style={{ color: "#fff", padding: 24 }}>Admin ordini</div>
-}
-
-function AdminNewOrder() {
-  return <div style={{ color: "#fff", padding: 24 }}>Nuovo ordine</div>
-}
-
-function AdminProducts() {
-  return <div style={{ color: "#fff", padding: 24 }}>Admin prodotti</div>
-}
+import AdminOrders from "./pages/AdminOrders"
+import AdminNewOrder from "./pages/AdminNewOrder"
+import AdminProducts from "./pages/AdminProducts"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* SITO PUBBLICO */}
         <Route path="/" element={<Home />} />
 
+        {/* ADMIN */}
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/orders" element={<AdminOrders />} />
         <Route path="/admin/orders/new" element={<AdminNewOrder />} />
